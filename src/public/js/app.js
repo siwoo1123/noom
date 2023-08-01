7ea7df5f12e6e5d0417228b2235edf24d3f1ee40
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2023-08-01 13:59:30 +0900

    nick1

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,13 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMsg(type, payload)
+{
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server");
@@ -19,7 +26,16 @@ socket.addEventListener("close", () => {
 function handleSubmit(event){
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeMsg('newMessage', input.value));
     input.value = "";
 }
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMsg('nickname', input.value));
+    input.value = "";
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
